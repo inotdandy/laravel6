9b510e933bdafaa40d9fdf83721eb5603086c5c8
Dandy Inot <dandy.inot@c9.com.au>
2022-07-14 01:51:07 +0800

    Save new database

--- database/laravel6.sql
+++ database/laravel6.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 26, 2022 at 06:00 PM
+-- Generation Time: Jul 13, 2022 at 07:48 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `articles` (
   `id` bigint(20) UNSIGNED NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
   `title` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `excerpt` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `body` text COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -40,15 +41,41 @@ CREATE TABLE `articles` (
 -- Dumping data for table `articles`
 --
 
-INSERT INTO `articles` (`id`, `title`, `excerpt`, `body`, `created_at`, `updated_at`) VALUES
-(1, 'Article title one', 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.', '2022-06-24 22:49:15', '2022-06-25 01:15:04'),
-(2, 'Article title two', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.', '2022-06-24 22:50:41', '2022-06-24 22:50:41'),
-(3, 'Article title three', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.', '2022-06-24 22:52:34', '2022-06-24 22:52:34'),
-(4, 'My new article four', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with', '2022-06-25 01:04:58', '2022-06-25 01:17:28'),
-(5, 'My new article five', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with', '2022-06-25 01:05:13', '2022-06-25 01:17:38'),
-(6, 'My six article', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.', 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', '2022-06-25 01:24:38', '2022-06-25 01:24:38'),
-(7, 'Article title seven', 'This is the excerpt of article seven', 'This is the body content of article seven', '2022-06-25 03:18:58', '2022-06-25 03:31:52'),
-(8, 'Article title eight', 'This is article eight', 'This is the body of article eight', '2022-06-25 03:21:27', '2022-06-25 03:28:57');
+INSERT INTO `articles` (`id`, `user_id`, `title`, `excerpt`, `body`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Consequatur nulla illum ad sequi illum et.', 'Voluptas reprehenderit ea maxime reiciendis.', 'Tempora mollitia et corporis ut. Autem aut ipsam eos exercitationem tempore et dolor sunt. Dignissimos quidem ut aut maxime. Ut voluptas soluta sed doloribus.', '2022-07-10 06:50:25', '2022-07-10 06:50:25'),
+(2, 1, 'Quod commodi iure voluptatem distinctio quia.', 'Eum est et ut fugiat.', 'Molestiae quia qui sed ipsum incidunt perferendis. Officia officia reprehenderit suscipit ipsum ab. Asperiores minus qui cupiditate similique consequatur voluptatem. Similique non rem quas totam qui.', '2022-07-10 06:50:25', '2022-07-10 06:50:25'),
+(3, 1, 'Odit suscipit corrupti atque quae a illum aut.', 'Atque nemo voluptatem quod nihil.', 'Molestias doloribus dolorem et magnam atque optio minima. Quae voluptatem sequi culpa corrupti quae. Accusamus repudiandae sint magni doloribus atque voluptatem enim.', '2022-07-10 06:50:25', '2022-07-10 06:50:25'),
+(4, 1, 'Quidem sint reiciendis minus minus repellat.', 'Ratione sint aut aut nulla illum accusamus.', 'Incidunt et perspiciatis iste cumque quia. Non quo rem neque quam eos modi quae eum. Id qui iste ut.', '2022-07-10 06:50:25', '2022-07-10 06:50:25'),
+(5, 1, 'Quidem rerum voluptate et dolore sequi ipsam hic.', 'Similique qui ad dolores eos et soluta.', 'Ea similique nulla accusantium rem et labore ut. Nobis occaecati quia voluptate non magnam totam. Sunt quis dolor exercitationem id velit ut. Voluptatem tenetur ut sit.', '2022-07-10 06:50:25', '2022-07-10 06:50:25'),
+(6, 2, 'Illum aut et laudantium.', 'Debitis culpa voluptate eos commodi praesentium.', 'Alias doloribus assumenda recusandae aperiam magni mollitia. Vitae laboriosam nemo ut. Sit aut et fugiat architecto quaerat ea magnam. Dolores recusandae consectetur magni.', '2022-07-10 06:50:39', '2022-07-10 06:50:39'),
+(7, 2, 'Nostrum quam aut necessitatibus porro dolorem.', 'Maxime veniam autem quia.', 'Sit et nostrum perspiciatis ex. Et et ad non deserunt et. Natus suscipit omnis delectus ab.', '2022-07-10 06:50:39', '2022-07-10 06:50:39'),
+(8, 2, 'Architecto quaerat nihil voluptates quae voluptatem labore.', 'Placeat vero ea est consequatur.', 'Velit itaque repellat molestiae est accusantium. Sequi quod consectetur iusto voluptate qui sit sit. Eos excepturi et voluptates non cumque. Consequatur officiis commodi aut voluptatem.', '2022-07-10 06:50:39', '2022-07-10 06:50:39'),
+(9, 1, 'Article with tags', 'this article is with tags', 'This article body has tags attach on it.', '2022-07-10 08:02:54', '2022-07-10 08:02:54'),
+(10, 1, 'Articles with tags', 'This articles has tags', 'This article body has tags attach on it', '2022-07-10 08:03:43', '2022-07-10 08:03:43');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `article_tag`
+--
+
+CREATE TABLE `article_tag` (
+  `article_id` bigint(20) UNSIGNED NOT NULL,
+  `tag_id` bigint(20) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `article_tag`
+--
+
+INSERT INTO `article_tag` (`article_id`, `tag_id`, `created_at`, `updated_at`) VALUES
+(1, 1, NULL, NULL),
+(1, 2, NULL, NULL),
+(1, 3, NULL, NULL),
+(10, 1, NULL, NULL),
+(10, 3, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -64,13 +91,6 @@ CREATE TABLE `assignments` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Dumping data for table `assignments`
---
-
-INSERT INTO `assignments` (`id`, `body`, `completed`, `due_date`, `created_at`, `updated_at`) VALUES
-(1, 'Learn laravel framework', 1, NULL, '2022-06-24 08:59:34', '2022-06-24 09:18:45');
 
 -- --------------------------------------------------------
 
@@ -108,8 +128,9 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
 (4, '2022_06_24_164416_create_posts_table', 1),
-(6, '2022_06_24_165446_create_assignments_table', 2),
-(7, '2022_06_25_064205_create_articles_table', 3);
+(5, '2022_06_24_165446_create_assignments_table', 1),
+(6, '2022_06_25_064205_create_articles_table', 1),
+(7, '2022_07_10_150805_create_tags_table', 2);
 
 -- --------------------------------------------------------
 
@@ -139,12 +160,27 @@ CREATE TABLE `posts` (
   `updated_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `posts`
+-- Table structure for table `tags`
 --
 
-INSERT INTO `posts` (`id`, `title`, `slug`, `body`, `published_at`, `created_at`, `updated_at`) VALUES
-(1, 'My first post', 'my-first-post', 'This is my first post', NULL, '2022-06-24 08:53:18', '2022-06-24 08:53:18');
+CREATE TABLE `tags` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `tags`
+--
+
+INSERT INTO `tags` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'Laravel', '2022-07-10 07:12:30', '2022-07-10 07:12:30'),
+(2, 'Work', '2022-07-10 07:13:08', '2022-07-10 07:13:08'),
+(3, 'Education', '2022-07-10 07:13:30', '2022-07-10 07:13:30');
 
 -- --------------------------------------------------------
 
@@ -164,6 +200,14 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
+(1, 'Marjolaine Hoeger', 'nayeli.walsh@example.org', '2022-07-10 06:49:36', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'dJ0nkt1SLR', '2022-07-10 06:49:36', '2022-07-10 06:49:36'),
+(2, 'Ebba Mann DDS', 'jerde.antone@example.com', '2022-07-10 06:49:36', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'QhB86tware', '2022-07-10 06:49:36', '2022-07-10 06:49:36');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -171,7 +215,15 @@ CREATE TABLE `users` (
 -- Indexes for table `articles`
 --
 ALTER TABLE `articles`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `articles_user_id_foreign` (`user_id`);
+
+--
+-- Indexes for table `article_tag`
+--
+ALTER TABLE `article_tag`
+  ADD UNIQUE KEY `article_tag_article_id_tag_id_unique` (`article_id`,`tag_id`),
+  ADD KEY `article_tag_tag_id_foreign` (`tag_id`);
 
 --
 -- Indexes for table `assignments`
@@ -204,6 +256,12 @@ ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `tags`
+--
+ALTER TABLE `tags`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -218,13 +276,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `articles`
 --
 ALTER TABLE `articles`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `assignments`
 --
 ALTER TABLE `assignments`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `failed_jobs`
@@ -242,13 +300,36 @@ ALTER TABLE `migrations`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tags`
+--
+ALTER TABLE `tags`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `articles`
+--
+ALTER TABLE `articles`
+  ADD CONSTRAINT `articles_user_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `article_tag`
+--
+ALTER TABLE `article_tag`
+  ADD CONSTRAINT `article_tag_article_id_foreign` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `article_tag_tag_id_foreign` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
